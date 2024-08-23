82d8932bd80e607e1a651a01e1817b14e08478d9
gp-kobayashi <kobayashi_y@gp1000.jp>
2024-08-23 16:24:49 +0900

    [fix]eslintの修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-    env: {
-      browser: true,
-      es6: true
-    },
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
-      "prettier",
-      "plugin:prettier/recommended",
-    ],
-    plugins: [],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      "sourceType": "module",
-      "project": "./tsconfig.json" 
-    },
-    root: true,
-    rules: {}
-  }
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended", 
+    "prettier", 
+    "prettier/@typescript-eslint",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    "sourceType": "module",
+    "project": "./tsconfig.json" 
+  },
+  root: true,
+  rules: {}
+}
