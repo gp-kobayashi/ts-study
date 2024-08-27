de500509c7cfe9864dace8b9466f6b93daef953c
gp-kobayashi <kobayashi_y@gp1000.jp>
2024-08-27 16:06:46 +0900

    [add]ジェネリック型を使ったポリモーフィズムの学習

--- src/index.ts
+++ src/index.ts
@@ -31,8 +31,14 @@
 // typeAliasSample()
 
 // 06.配列とタプルの型定義
-import arraySample from './array/array'
-import tupleSample from './array/tuple'
+// import arraySample from './array/array'
+// import tupleSample from './array/tuple'
 
-arraySample()
-tupleSample()
+// arraySample()
+// tupleSample()
+
+// 07.ジェネリック型とポリモーフィズム
+import genericsBasicSample from './generics/basic'
+import genericAdvanceSample from './generics/advanced'
+genericsBasicSample()
+genericAdvanceSample()
